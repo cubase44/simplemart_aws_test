07960d67d6734f21ac6b715dc29b6ed6416ad304
cubase44 <cubase44unlock333@gmail.com>
2024-01-06 20:20:08 +0900

    新規登録完了画面の実装

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -7,9 +7,9 @@ class SignupController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to root_path, notice: "アカウトを作成しました。"
+            render :create, status: :unprocessable_entity
         else
-            render :new
+            render :new, status: :unprocessable_entity
         end
     end
 
