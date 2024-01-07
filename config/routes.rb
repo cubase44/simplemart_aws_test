d1980d72a23c98f7f23381a975e9c8c4573c64f2
cubase44 <cubase44unlock333@gmail.com>
2024-01-07 19:24:24 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  root to:'homes#index'
+  root to: "homes#index"
 
-  get "signup", to: "signup#new"
-  post "signup", to: "signup#create"
+  get    "signup",    to: "signup#new"
+  post   "signup",    to: "signup#create"
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    "login",     to: "sessions#new"
+  post   "login",     to: "sessions#create"
+  delete "logout",    to: "sessions#destroy"
+
+  get    "items",     to: "items#index"
+  get    "items/:id", to: "items#show"
+
+  get    "carts",     to: "carts#index"
+  post   "carts",     to: "carts#create"
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
